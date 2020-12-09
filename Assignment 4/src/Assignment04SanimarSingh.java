43fbed9d7a631ce050997563cee62a4ea912c875
SanimarSingh <SanimarSingh@users.noreply.github.com>
2020-12-08 19:59:04 -0500

    Added programmer comments, and starter code to method main.
    Signed-off-by: SanimarSingh <SanimarSingh@users.noreply.github.com>

--- Assignment 4/src/Assignment04SanimarSingh.java
+++ Assignment 4/src/Assignment04SanimarSingh.java
@@ -1,9 +1,21 @@
 
 public class Assignment04SanimarSingh {
 
+	//main class
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ 
+		//array
+			int[] primes = { 2, 3, 5, 7 };
+			// for loop
+			for(int index = 0; index < primes.length; index++) 
+			{
+		// print array
+				System.out.println(primes[index]);
+		}
+		
+			// author
+			System.out.println("Program by Sanimar Singh");
 	}
 
 }
